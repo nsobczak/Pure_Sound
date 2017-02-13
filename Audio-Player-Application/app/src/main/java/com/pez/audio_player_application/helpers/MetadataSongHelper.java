a3b72db16b7e066421e3f2148c6151358fc5e367
Baudouin Landais <baudouin.landais@isen-lille.fr>
2017-02-13 22:12:57 +0100

    première version, clé api lastfm

---
+++ Audio-Player-Application/app/src/main/java/com/pez/audio_player_application/helpers/MetadataSongHelper.java
@@ -0,0 +1,15 @@
+package com.pez.audio_player_application.helpers;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Baudouin on 13/02/2017.
+ */
+
+public class MetadataSongHelper {
+
+    public static void getAlbumCover(String albumTitle, String artistName) {
+
+    }
+
+}
---
+++ Audio-Player-Application/app/src/main/java/com/pez/audio_player_application/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.pez.audio_player_application.utils;
+
+/**
+ * Created by Baudouin on 13/02/2017.
+ */
+
+public class Constants {
+}
